0558dcec3d744a9fb8c247e0ace53ff2be139e30
1Cezzo <emiljohnzen@gmail.com>
2023-11-07 18:48:24 +0100

    Created login page and system for saving login credentials

---
+++ pages/api/saveLogin.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+export default (req, res) => {
+  if (req.method === 'POST') {
+    const dataPath = path.join(process.cwd(), 'data', 'users.json');
+    const { email, password } = req.body;
+
+    // Read the existing user data from users.json
+    const usersData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+    // Create a new user object with the email and password
+    const newUser = { email, password };
+
+    // Append the new user to the existing data
+    usersData.push(newUser);
+
+    // Write the updated data back to users.json
+    fs.writeFileSync(dataPath, JSON.stringify(usersData), 'utf-8');
+
+    // Respond with a success message
+    res.status(200).json({ message: 'Login information saved successfully' });
+  } else {
+    res.status(405).end(); // Method Not Allowed for non-POST requests
+  }
+};
