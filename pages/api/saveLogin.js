140e31c3e1745016f03ba169a10e2ded5241c408
1Cezzo <emiljohnzen@gmail.com>
2023-11-08 00:34:00 +0100

    testing deployment

--- pages/api/saveLogin.js
+++ pages/api/saveLogin.js
@@ -21,6 +21,24 @@ export default (req, res) => {
     // Respond with a success message
     res.status(200).json({ message: 'Login information saved successfully' });
   } else {
-    res.status(405).end(); // Method Not Allowed for non-POST requests
+    if (req.method === 'POST') {
+    const dataPath = path.join(process.cwd(), 'data', 'users.json');
+    const { email, password } = req.body;
+
+    // Read the existing user data from users.json
+    const usersData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+    // Create a new user object with the email and password
+    const newUser = { email, password };
+
+    // Append the new user to the existing data
+    usersData.push(newUser);
+
+    // Write the updated data back to users.json
+    fs.writeFileSync(dataPath, JSON.stringify(usersData), 'utf-8');
+
+    // Respond with a success message
+    res.status(200).json({ message: 'Login information saved successfully' });
   }
+}
 };
