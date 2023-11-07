9d3dc3b119e9a17f69fd39245f1c2f123a5cc663
1Cezzo <emiljohnzen@gmail.com>
2023-11-08 00:46:12 +0100

    testing deployment

--- pages/api/saveLogin.js
+++ pages/api/saveLogin.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import Cors from 'micro-cors';
+const cors = Cors();
 
-export default (req, res) => {
+export default cors((req, res) => {
   if (req.method === 'POST') {
     const dataPath = path.join(process.cwd(), 'data', 'users.json');
     const { email, password } = req.body;
@@ -21,24 +23,6 @@ export default (req, res) => {
     // Respond with a success message
     res.status(200).json({ message: 'Login information saved successfully' });
   } else {
-    if (req.method === 'POST') {
-    const dataPath = path.join(process.cwd(), 'data', 'users.json');
-    const { email, password } = req.body;
-
-    // Read the existing user data from users.json
-    const usersData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
-
-    // Create a new user object with the email and password
-    const newUser = { email, password };
-
-    // Append the new user to the existing data
-    usersData.push(newUser);
-
-    // Write the updated data back to users.json
-    fs.writeFileSync(dataPath, JSON.stringify(usersData), 'utf-8');
-
-    // Respond with a success message
-    res.status(200).json({ message: 'Login information saved successfully' });
+    res.status(405).end(); // Method Not Allowed for non-POST requests
   }
-}
-};
+});
